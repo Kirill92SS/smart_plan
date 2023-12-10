58e1c5039e8dc55c2ce4e27b6a78835f055b1ea1
Kirill92SS <p.k.a.1992@yandex.ru>
2023-12-10 15:36:05 +0300

    Remove all __pycache__ from tracking list

--- plan_main/structure/models.py
+++ plan_main/structure/models.py
@@ -4,14 +4,9 @@ from django.utils import timezone
 
 class BaseModel(models.Model):
     name = models.CharField(max_length=256)
-    guid = models.CharField(max_length=36, blank=True)
+    guid = models.CharField(max_length=36, null=True)
     created = models.DateTimeField(auto_now_add=True)
-    edited = models.DateTimeField(default=None, blank=True, null=True)
-
-    def save(self, *args, **kwargs):
-        if not self.edited:
-            self.edited = timezone.now()
-        super().save(*args, **kwargs)
+    edited = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -24,7 +19,7 @@ class Factory(BaseModel):
     long_name = models.CharField(max_length=256)
     city = models.CharField(max_length=100)
     address = models.CharField(max_length=256)
-    f_address = models.CharField(max_length=256)
+    f_address = models.CharField(max_length=256, null=True)
 
 
 class DepartmentType(BaseModel):
@@ -33,7 +28,9 @@ class DepartmentType(BaseModel):
 
 class Department(BaseModel):
     factory = models.ForeignKey('Factory', on_delete=models.PROTECT)
-    id_parent = models.IntegerField(blank=True)
+    id_parent = models.IntegerField(null=True)
+    id_parent_guid = models.CharField(max_length=36, null=True)
     date_begin = models.DateField()
-    date_end = models.DateField(blank=True)
+    date_end = models.DateField(null=True)
     department_type = models.ForeignKey('DepartmentType', on_delete=models.PROTECT)
+    id_department_type = models.CharField(max_length=36, null=True)
